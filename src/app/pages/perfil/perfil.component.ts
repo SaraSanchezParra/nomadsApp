1780ea844fda22bdff2dc009d8a43e737ae416a0
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-04 20:56:47 +0200

    cambios finales

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -13,6 +13,7 @@ export class PerfilComponent {
   public user = { name: '', surname: '', email: '', username: '', descripcion:"",photo:'', favs: [], misviajes: [] };
 
   public showFavs = true;
+  public showIcons = false;
   
   
   constructor() {
@@ -25,14 +26,14 @@ export class PerfilComponent {
       descripcion: "Hola, me llamo Stefi y soy una viajera empedernida.",
       photo:'https://www.sevillainfo.es/wp-content/uploads/2022/05/IMG-0810.jpg',
       favs: [
-        {titulo:'Malaga Costa', descripcion:'Visita la costa del Sol y nunca querras irte', likes:290},
+        {titulo:'Málaga Costa', descripcion:'Visita la costa del Sol y nunca querrás irte', likes:290},
         {titulo:'Sevilla', descripcion:'Disfruta de su color y su encanto', likes:120},
-        {titulo:'Madrid Centro', descripcion:'Visita la costa del Sol y nunca querras irte', likes:260}
+        {titulo:'Madrid Centro', descripcion:'Como siempre, de Madrid al cielo', likes:600}
       ],
       misviajes: [
-        {titulo:'Madrid Centro', descripcion:'Visita la costa del Sol y nunca querras irte', likes:260},
-        {titulo:'Porrua', descripcion:'Visita la costa del Sol y nunca querras irte', likes:490},
-        {titulo:'Malaga Costa', descripcion:'Visita la costa del Sol y nunca querras irte', likes:290}
+        {titulo:'Salamanca', descripcion:'Piérdete por sus calles', likes:260},
+        {titulo:'Porrua', descripcion:'El pueblo con más encanto de España', likes:490},
+        {titulo:'Galicia', descripcion:'Su gente, su marisco, sus paisajes...CALIDADE', likes:300}
       ]
     };
     console.log(this.user);
@@ -42,10 +43,12 @@ export class PerfilComponent {
   
   public mostrarFavoritas(): void {
     this.showFavs = true;
+    this.showIcons = false;
   }
   
   public mostrarMisViajes(): void {
    this.showFavs = false;
+   this.showIcons = true;
   }
 }
 
