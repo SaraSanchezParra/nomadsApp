fda844ffbbbbab6c47f3dc552b2d964ec519f196
juliop11 <julioperezobrero@hotmail.com>
2023-04-04 15:14:06 +0200

    home sin loged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeNoLogedComponent } from './pages/home-no-loged/home-no-loged.component';
+
 
 const routes: Routes = [
-  {path: "home-no-loged", component: HomeNoLogedComponent}
+
 ];
 
 @NgModule({
