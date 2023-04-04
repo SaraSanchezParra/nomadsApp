03b1c0583074b4973d022e133084adccd111c4e2
Estefania-Cueva <estefaniacuevavivas@gmail.com>
2023-04-04 12:03:44 +0200

    paginaRegister

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/models/user';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  public user = new User;
+  public myForm: FormGroup;
+  public router: Router
+
+  constructor(private formBuilder:FormBuilder){
+    this.buildForm()
+  }
+
+  public register(){
+    const user = this.myForm.value;
+    console.log(user);
+    this.router.navigateByUrl('/login')
+  }
+
+  private buildForm(){
+    const minPassLength = 8;
+  
+    this.myForm = this.formBuilder.group({
+      nombreUsuario: ['', [Validators.required, Validators.pattern(/^@/)]],
+      email: ['', [Validators.required, Validators.email]],
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      foto: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(minPassLength)]],
+      password2: ['', [Validators.required, this.checkPasswords]]
+    });
+  }
+  
+
+  private checkPasswords(control: AbstractControl)
+  {
+    let resultado = {matchPassword: true};
+
+    // console.log(control.parent);
+    // console.log(control.parent?.value);
+    // console.log(control);
+
+
+    if (control.parent?.value.password == control.value)
+      resultado = null;
+
+    return resultado;
+  }
 
 }
