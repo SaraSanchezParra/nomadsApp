66ddc9977e09a1f26f88cd7c042d4cb263ab6953
juliop11 <julioperezobrero@hotmail.com>
2023-04-05 18:01:34 +0200

    modificar home

--- src/app/components/card-viaje/card-viaje.component.ts
+++ src/app/components/card-viaje/card-viaje.component.ts
@@ -11,6 +11,7 @@ export class CardViajeComponent {
   viaje: Viaje
 
 constructor() {
+  
   this.viaje = new Viaje('Madrid Centro', 'https://media.istockphoto.com/id/1059076792/es/foto/madrid-ciudad-skyline-gran-v%C3%ADa-calle-crep%C3%BAsculo-espa%C3%B1a.jpg?s=612x612&w=0&k=20&c=gWGpBRHVNJleHrVRmetRPTFuau_aahvCUKDMNfCrMNE=', 'El corazon de la capital', [], 490)
 }
 }
