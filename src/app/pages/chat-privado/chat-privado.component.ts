f129221e4aa0fa99d99a5e2ecedbee3b0d61e600
Estefania-Cueva <estefaniacuevavivas@gmail.com>
2023-04-05 12:21:30 +0200

    chatPrivado

--- src/app/pages/chat-privado/chat-privado.component.ts
+++ src/app/pages/chat-privado/chat-privado.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-chat-privado',
   templateUrl: './chat-privado.component.html',
   styleUrls: ['./chat-privado.component.css']
 })
 export class ChatPrivadoComponent {
+
+ 
 
 }
