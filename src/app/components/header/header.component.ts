6b7eb48dfd4fd66876309a8c9a2f828802ab8ba4
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-05 15:36:26 +0200

    ngIf header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public loged : Boolean;
+
+  constructor(){
+
+    this.loged = true;
+    console.log(this.loged);
+    
+  }
 }
