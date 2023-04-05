d1bed1a3cd9a5cf828435935ca395ffb289721cf
Rebeca Rello <rebeca.rello@gmail.com>
2023-04-05 11:37:29 +0200

    busqueda

--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class BusquedaComponent {
 
+  constructor(private router: Router) { }
+
+  navigateToRegister() {
+    this.router.navigate(['/login']);
+  }
 }
