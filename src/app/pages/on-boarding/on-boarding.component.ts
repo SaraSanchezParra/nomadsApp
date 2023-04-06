c8faf6c5a9cdf9e44ca4bdf700e569ec7c0babb7
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-06 20:58:17 +0200

    modificaciones

--- src/app/pages/on-boarding/on-boarding.component.ts
+++ src/app/pages/on-boarding/on-boarding.component.ts
@@ -1,4 +1,8 @@
+
 import { Component } from '@angular/core';
+import { Router} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-on-boarding',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./on-boarding.component.css']
 })
 export class OnBoardingComponent {
+  
+  currentPage = 0;
 
+  constructor(private router: Router) {}
+
+
+
+  goToPreviousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
+
+  goToNextPage(): void {
+    if (this.currentPage < 5) {
+      this.currentPage++;
+      console.log("Página actual: ", this.currentPage);
+    }
+  }
+
+  goToHome() {
+    const isLogged = sessionStorage.getItem('isLogged');
+    if (isLogged === 'true') {
+      this.router.navigate(['/home-logged']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+  // goToHome() {
+  //   const isLogged = sessionStorage.getItem('isLogged');
+  //   if (isLogged === 'false') {
+  //     this.router.navigate(['/home-logged']);
+  //   } else {
+  //     this.router.navigate(['/login']);
+  //   }
+  // }
+  
+  
 }
