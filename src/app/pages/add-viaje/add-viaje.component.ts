b0ca38a21c9940948345bb68358a6fa62913d797
Amber C <amberpomar@gmail.com>
2023-04-06 11:35:40 +0200

    add viajes

--- src/app/pages/add-viaje/add-viaje.component.ts
+++ src/app/pages/add-viaje/add-viaje.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Day } from 'src/app/models/day';
+import { Viaje } from 'src/app/models/viaje';
 
 @Component({
   selector: 'app-add-viaje',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AddViajeComponent {
 
+  public viaje: Viaje;
+  public addForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm()
+  }
+
+  private buildForm() {
+    this.addForm = this.formBuilder.group({
+      nombreViaje: ['', Validators.required],
+      descripcionViaje: ['', Validators.required],
+      fotoViaje: ['', Validators.required]
+    })
+  }
 }
