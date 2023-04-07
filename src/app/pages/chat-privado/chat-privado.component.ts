ede0afd9d6c8cd1acc83748d0b8ee78d733d6383
Estefania-Cueva <estefaniacuevavivas@gmail.com>
2023-04-07 11:29:30 +0200

    chatPrivado

--- src/app/pages/chat-privado/chat-privado.component.ts
+++ src/app/pages/chat-privado/chat-privado.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 
@@ -10,7 +11,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-privado.component.css']
 })
 export class ChatPrivadoComponent {
+  mensajeActual: string = '';
+  mensajes: string[] = [];
+  mensajePredefinido = 'Hola, ¿cómo estás?';
+  mensajePredefinido2 = 'Hola, bien y tu como estas';
+  haRespondidoPredefinido: boolean = false;
+  bocadilloHeight = '25%';
+  mostrarBloquear = false;
+  chatBloqueado = false;
+  habilitarBotonBloquear = true;
 
- 
 
+  constructor(private router: Router,private elementRef: ElementRef){
+    this.chatBloqueado = false;
+  }
+  
+  navegarAChats() {
+    this.router.navigate(['/chat-general']);
+  }
+
+  enviarMensaje() {
+    if (this.mensajeActual.includes(this.mensajePredefinido2) && !this.haRespondidoPredefinido) {
+      this.mensajes.push('Estoy bien, gracias.');
+      this.haRespondidoPredefinido = true; // establecer haRespondidoPredefinido a true
+    } else {
+      this.mensajes.push(this.mensajeActual);
+    }
+    
+    this.mensajeActual = '';
+  }
+  
+
+  toggleMenu() {
+    this.mostrarBloquear = !this.mostrarBloquear;
+  }
+  bloquearChat() {
+    const chat = this.elementRef.nativeElement.querySelector('.chat');
+    if (this.chatBloqueado) {
+      chat.classList.remove('chat-bloqueado');
+      this.habilitarBotonBloquear = true;
+    } else {
+      chat.classList.add('chat-bloqueado');
+      this.habilitarBotonBloquear = false;
+    }
+    this.chatBloqueado = !this.chatBloqueado;
+    this.mostrarBloquear = false;
+  }
 }
