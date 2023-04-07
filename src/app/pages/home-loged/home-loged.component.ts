8e5159b9192a2f45b61802ef04e5f6f4b676238c
Amber C <amberpomar@gmail.com>
2023-04-07 12:23:53 +0200

    modificar viaje

--- src/app/pages/home-loged/home-loged.component.ts
+++ src/app/pages/home-loged/home-loged.component.ts
@@ -10,11 +10,11 @@ export class HomeLogedComponent {
 
   inputValue: string = '';
   
-  public viaje: Viaje[];
+  public viajes: Viaje[];
 
   constructor() {
 
-    this.viaje = [
+    this.viajes = [
       new Viaje('Madrid Centro', 'https://media.istockphoto.com/id/1059076792/es/foto/madrid-ciudad-skyline-gran-v%C3%ADa-calle-crep%C3%BAsculo-espa%C3%B1a.jpg?s=612x612&w=0&k=20&c=gWGpBRHVNJleHrVRmetRPTFuau_aahvCUKDMNfCrMNE=', 'El corazon de la capital', [], 490),
       new Viaje('Málaga Costa', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRZOWEzyLzR19fys_Pvk3AIsWcXpmxZdcSfQ&usqp=CAU', 'Visita la costa del sol y nunca querrás irte.', [], 632),
       new Viaje('Sevilla', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpE7U8eEMWBvLEGpvY9HRIOr693Bih4e-tSg&usqp=CAU', 'Disfruta de su color y su encanto.', [], 356)
