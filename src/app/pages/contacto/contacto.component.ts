ed9daa5151d1c10fa2e094e1ed766e46ac688269
juliop11 <julioperezobrero@hotmail.com>
2023-04-08 13:07:23 +0200

    validadores contactanos

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class ContactoComponent {
 
+  public myForm: FormGroup;
+
+
+  constructor(private formBuilder:FormBuilder){
+    this.buildForm()
+  }
+
+
+  private buildForm(){
+   
+  
+    this.myForm = this.formBuilder.group({
+
+      nombre: ['', Validators.required],
+      asunto: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+     
+     
+    });
+  }
+  
 }
