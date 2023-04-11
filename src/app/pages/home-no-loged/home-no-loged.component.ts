6a54cffae82abde0a24ffab07faee789c1d7c7c5
juliop11 <julioperezobrero@hotmail.com>
2023-04-11 15:05:17 +0200

    modificaciones varias

--- src/app/pages/home-no-loged/home-no-loged.component.ts
+++ src/app/pages/home-no-loged/home-no-loged.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Viaje } from 'src/app/models/viaje';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home-no-loged',
@@ -9,15 +11,24 @@ import { Viaje } from 'src/app/models/viaje';
 export class HomeNoLogedComponent {
 
   inputValue: string = '';
-  
+
   public viajes: Viaje[];
 
-  constructor() {
+  constructor(private router: Router, private route: ActivatedRoute) {
 
     this.viajes = [
       new Viaje('Madrid Centro', 'https://media.istockphoto.com/id/1059076792/es/foto/madrid-ciudad-skyline-gran-v%C3%ADa-calle-crep%C3%BAsculo-espa%C3%B1a.jpg?s=612x612&w=0&k=20&c=gWGpBRHVNJleHrVRmetRPTFuau_aahvCUKDMNfCrMNE=', 'El corazon de la capital', [], 490),
       new Viaje('Málaga Costa', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReKtus2s69K5sGyRDm6e_ybvjZBOBkaM2cOw&usqp=CAU', 'Visita la costa del sol y nunca querrás irte.', [], 632),
       new Viaje('Sevilla', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpE7U8eEMWBvLEGpvY9HRIOr693Bih4e-tSg&usqp=CAU', 'Disfruta de su color y su encanto.', [], 356)
     ];
+
+
   }
+ destino(): void {
+    this.router.navigate(['/viajesDestino']);
+  }
+
 }
+
+
+
