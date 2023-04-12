6089e9da454ae5916f82e018416f74131865ebbe
Amber C <amberpomar@gmail.com>
2023-04-12 12:14:48 +0200

    Map

--- src/app/pages/pagina-viaje/pagina-viaje.component.ts
+++ src/app/pages/pagina-viaje/pagina-viaje.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, tileLayer } from 'leaflet';
 import { Day } from 'src/app/models/day';
 import { PuntoDeInteres } from 'src/app/models/punto-de-interes';
 import { Viaje } from 'src/app/models/viaje';
@@ -19,6 +20,14 @@ constructor() {
   this.dia = new Day([new PuntoDeInteres('https://d1bvpoagx8hqbg.cloudfront.net/originals/nature-madrid-480758233de4362f5443491d3b2e55d9.jpg', 'parque del Retiro'), new PuntoDeInteres('https://lonelyplanetimages.imgix.net/a/g/hi/t/fed1a2b677245627be71a1a62f48fbbc-museo-del-prado.jpg', 'El Prado')])
   this.viaje = new Viaje('Madrid de los Austrias', 'https://depaseo.eu/wp-content/uploads/2018/09/visita-austrias-A.jpg', 'madrid del siglo de oro', [this.dia, new Day([new PuntoDeInteres('https://www.taquilla.com/data/images/t/27/jardin-botanico.jpg', 'Jardin Botanico')])], 13)
   this.fav=false
+}
+
+ngAfterViewInit(): void {
+  const map = new Map('map').setView([51.505, -0.09], 13);
+  tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 }
 
 checkfav():boolean {
