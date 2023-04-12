35212ee04953fcd3d998eb7b8c51c7c6687b9c2d
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-12 15:29:52 +0200

    creada rama

--- src/app/models/respuesta.ts
+++ src/app/models/respuesta.ts
@@ -1,6 +1,9 @@
 
 import { Viaje } from "src/app/models/viaje";
 import { User } from "./user";
+import { Chats } from "./chat";
+import { ChatPrivado } from "./chatPrivado";
+
 
 export class Respuesta {
 
@@ -8,8 +11,13 @@ export class Respuesta {
     public error: boolean,
     public codigo: number,
     public mensaje: string,
-    public data_viaje: Viaje,
-    public data_user: User,
+    public data_viaje: Viaje[],
+    public data_user: User[], //En caso de que solo se vaya a usar un suaurio, especificar data_user[0] para acceder solo al primero.
+    public data_chat: Chats,
+    public chatPrivado: ChatPrivado[]
+    
+
+
     
   ){}
 
