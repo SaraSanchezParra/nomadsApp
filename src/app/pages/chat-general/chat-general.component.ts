ee1d67d4cafd4444a1c1820c2183c0162ed8d4b7
Rebeca Rello <rebeca.rello@gmail.com>
2023-04-12 17:27:32 +0200

    cambios en front

--- src/app/pages/chat-general/chat-general.component.ts
+++ src/app/pages/chat-general/chat-general.component.ts
@@ -9,68 +9,94 @@ import { Chats } from 'src/models/chat';
 })
 export class ChatGeneralComponent {
   public formattedTime: string;
-  usuarioNoEncontrado = false;
-  chatPrincipal=false;
-  chats: Chats[] = [
-    {
-      photo: 'assets/mujerHermosa.jpg',
-      nameUser: '@Maria',
-      hour: "12:00"
-    },
-    {
-      photo: 'https://img.freepik.com/foto-gratis/chico-worldface-espanol-fondo-blanco_53876-137665.jpg?w=2000',
-      nameUser: '@pablo',
-      hour: "01:20"
-    },
+  public indexEncontrado: number;
+  public usuarioEncontrado = false;
+  public chatPrincipal= true;
+  public chats: Chats[];
+  public chatBuscado: Chats | undefined;
+  public textoBusqueda = '';
+  public chatsMostrados: Chats[];
+  public buscandoUsuario: boolean;
 
-    {
-      photo: 'https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/09/Resena-La-chica-salvaje-El-misterio-es-el-nuevo-lenguaje-del-amor.jpg?fit=1280%2C720&quality=80&ssl=1',
-      nameUser: '@Ana',
-      hour: "14:15"
-    },
-    {
-      photo: 'https://img.freepik.com/foto-gratis/chico-worldface-espanol-fondo-blanco_53876-137665.jpg?w=2000',
-      nameUser: '@Ruben',
-      hour: "01:20"
-    },
-    {
-      photo: 'https://d1bvpoagx8hqbg.cloudfront.net/259/69f3b9690c1ff04fb18b212b5b3aec28.jpg',
-      nameUser: '@juan',
-      hour: "01:20"
-    },
-    {
-      photo: 'https://d1bvpoagx8hqbg.cloudfront.net/259/58c26f1727af2a4080a282cfb893d7ee.jpg',
-      nameUser: '@David',
-      hour: "01:20"
-    },
-    {
-      photo: 'https://static-cdn.jtvnw.net/jtv_user_pictures/8794d08c-6f89-4353-a6ac-f0492d9dc731-profile_image-300x300.png',
-      nameUser: '@Violeta',
-      hour: "01:20"
-    },
-
-  ]
-
-  chatBuscado: Chats | undefined;
-  textoBusqueda = '';
+  constructor ()
+  {
+    this.chats = [
+      {
+        photo: 'assets/mujerHermosa.jpg',
+        nameUser: '@Maria',
+        hour: "12:00"
+      },
+      {
+        photo: 'https://img.freepik.com/foto-gratis/chico-worldface-espanol-fondo-blanco_53876-137665.jpg?w=2000',
+        nameUser: '@pablo',
+        hour: "01:20"
+      },
+  
+      {
+        photo: 'https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/09/Resena-La-chica-salvaje-El-misterio-es-el-nuevo-lenguaje-del-amor.jpg?fit=1280%2C720&quality=80&ssl=1',
+        nameUser: '@Ana',
+        hour: "14:15"
+      },
+      {
+        photo: 'https://img.freepik.com/foto-gratis/chico-worldface-espanol-fondo-blanco_53876-137665.jpg?w=2000',
+        nameUser: '@Ruben',
+        hour: "01:20"
+      },
+      {
+        photo: 'https://d1bvpoagx8hqbg.cloudfront.net/259/69f3b9690c1ff04fb18b212b5b3aec28.jpg',
+        nameUser: '@juan',
+        hour: "01:20"
+      },
+      {
+        photo: 'https://d1bvpoagx8hqbg.cloudfront.net/259/58c26f1727af2a4080a282cfb893d7ee.jpg',
+        nameUser: '@David',
+        hour: "01:20"
+      },
+      {
+        photo: 'https://static-cdn.jtvnw.net/jtv_user_pictures/8794d08c-6f89-4353-a6ac-f0492d9dc731-profile_image-300x300.png',
+        nameUser: '@Violeta',
+        hour: "01:20"
+      },
+  
+    ]
+    this.chatsMostrados = this.chats;
+    this.buscandoUsuario = false;
+    this.chatPrincipal = true;
+  }
 
   buscarUsuario() {
     const nombreBusqueda = this.textoBusqueda.trim().toLowerCase();
-    this.chatBuscado = this.chats.find(chat => chat.nameUser.toLowerCase().includes(nombreBusqueda));
-    if (!this.chatBuscado) {
-      this.chatPrincipal =true
-      this.usuarioNoEncontrado = true;
-    } else {
-      this.chatPrincipal =true
-      this.usuarioNoEncontrado = false;
+    console.log(nombreBusqueda);
+    this.buscandoUsuario = true;
+    this.chatPrincipal = false;
+    this.indexEncontrado = -1;
+  
+    if (nombreBusqueda!="")
+      this.indexEncontrado = this.chats.findIndex(chat => chat.nameUser.toLowerCase().includes(nombreBusqueda));
+
+    console.log(this.indexEncontrado);
+    
+    this.usuarioEncontrado = (this.indexEncontrado != -1);
+    
+    if (this.usuarioEncontrado)
+    {
+      this.chatsMostrados = [this.chats[this.indexEncontrado]];
     }
+    else
+    {
+      this.chatsMostrados = [];
+    }
+
+    console.log (this.chatsMostrados);
   }
 
   resetearBusqueda() {
     this.chatBuscado = undefined;
     this.textoBusqueda = '';
-    this.usuarioNoEncontrado= false;
-    this.chatPrincipal=false;
+    this.usuarioEncontrado= false;
+    this.chatPrincipal=true;
+    this.buscandoUsuario = false; 
+    this.chatsMostrados = this.chats;
   }  
 
   // mostrarBotonBusqueda() {
@@ -84,12 +110,25 @@ export class ChatGeneralComponent {
   //   }
   
   // }
-  eliminarTarjeta(chat: Chats) {
-    const index = this.chats.indexOf(chat);
+  eliminarTarjeta(index: number) {
+    console.log(index);
     if (index !== -1) {
       this.chats.splice(index, 1);
-    
     }
+
+    if (this.chatsMostrados.length == 1)
+    {
+      this.chatsMostrados = [];
+    }
+    else
+    {
+      {
+        this.chatsMostrados = this.chats;
+      }
+    }
+
+    console.log("Hola que tal");
+    console.log(this.chats);
   }
   // constructor() {
   //   const now: Date = new Date();
