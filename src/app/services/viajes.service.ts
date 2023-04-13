e5132a4c07500dfc3515e446f8ac3b6b1c4fbdd4
juliop11 <julioperezobrero@hotmail.com>
2023-04-13 17:55:14 +0200

    funcionalidad home log

--- src/app/services/viajes.service.ts
+++ src/app/services/viajes.service.ts
@@ -8,13 +8,22 @@ import { Viaje } from 'src/app/models/viaje';
 })
 export class ViajesService {
 
- private apiUrl = "http://localhost:3000/viajes";
+  private apiUrl = "http://localhost:3000/viajes";
+  private apiUrl2 = "http://localhost:3000/viajesLog";
 
- constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
- getViajes(): Observable<Viaje[]> {
-   return this.http.get<Viaje[]>(this.apiUrl);
-   
- }
+  getViajes(): Observable<Viaje[]> {
+    return this.http.get<Viaje[]>(this.apiUrl);
+
+  }
+
+
+  getViajesLog(): Observable<Viaje[]> {
+    return this.http.get<Viaje[]>(this.apiUrl2);
+
+  }
+
+
 }
 
