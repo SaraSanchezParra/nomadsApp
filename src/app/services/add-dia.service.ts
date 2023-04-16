cdfad8732ad3c15592a63eb0f0f65a50cb889bee
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-16 12:31:54 +0200

    subida rama

--- src/app/services/add-dia.service.ts
+++ src/app/services/add-dia.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Viaje } from '../models/viaje';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddDiaService {
-  
+  private url = "http://localhost:3000/addDia";
+  private url2 = "http://localhost:3000/addPI"
   diaToAdd: Viaje
   
-  constructor() {
+  constructor(private http: HttpClient) {
     this.diaToAdd = new Viaje('', '', '', [], 0)
    }
+
+   postDia(newDay :Viaje){
+    console.log(newDay);
+    
+    return this.http.post(this.url, newDay)
+   }
+
+   postPI(newPI :Viaje){
+    console.log(newPI);
+    
+    return this.http.post(this.url2, newPI)
+   }
+
+
 }
