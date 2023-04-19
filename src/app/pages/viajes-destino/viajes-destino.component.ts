c56f0694aaf1945666926c80f0eceb213884380b
Rebeca Rello <rebeca.rello@gmail.com>
2023-04-19 17:52:36 +0200

    cambios en foto perfil hechos2

--- src/app/pages/viajes-destino/viajes-destino.component.ts
+++ src/app/pages/viajes-destino/viajes-destino.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Viaje } from 'src/app/models/viaje';
+import { DatosUsuarioService } from 'src/app/services/datos-usuario.service';
 import { ViajesService } from 'src/app/services/viajes.service';
 import { ViajeService } from 'src/app/shared/viaje.service';
 
@@ -14,13 +15,19 @@ export class ViajesDestinoComponent {
 
   viajesADestino: Viaje[]
   
-  constructor(public viajesService:ViajesService, public viajeService: ViajeService, public router: Router) { 
+  constructor(public viajesService:ViajesService, public viajeService: ViajeService, public router: Router, public userService:DatosUsuarioService) { 
 
     this.viajesADestino = this.viajesService.viajesBuscados
    
   }
 
   clickCard(viaje_id: number) {
-    this.viajeService.goToViaje(viaje_id);
+    if(this.userService.loged)
+    this.viajeService.goToViaje(viaje_id)
+    else{
+      this.userService.showHeaderFooter = false
+      this.router.navigate(["/login"])
+
+    }
   }
 }
