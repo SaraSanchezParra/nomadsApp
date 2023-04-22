2bcb65a628f9c88e9b2ce13cf7bc4ff7f76bc9e7
juliop11 <julioperezobrero@hotmail.com>
2023-04-22 11:32:24 +0200

    comprobar username

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -13,50 +13,50 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegisterComponent {
 
-  public user = new User ("","","","","","",[],[]);
+  public user = new User("", "", "", "", "", "", [], []);
   public myForm: FormGroup;
   public route: Router
   isFormValid: boolean = false;
-public username:User
+  public username: User
 
-  constructor(private formBuilder:FormBuilder,private router: Router,public userService:DatosUsuarioService,  private toastr: ToastrService, private registerservice: RegisterService){
+  constructor(private formBuilder: FormBuilder, private router: Router, public userService: DatosUsuarioService, private toastr: ToastrService, private registerservice: RegisterService) {
     this.buildForm();
     this.route = router;
   }
 
 
 
-public register() {
-  const user = this.myForm.value;
-  console.log(user);
+  public register() {
+    const user = this.myForm.value;
+    console.log(user);
 
-  // Verificar si el nombre de usuario ya existe
-  this.userService.usuarioEncontrado(user.username).subscribe(
-    (resp: any) => {
-      if (resp.length > 0) {
-        console.log(resp);
-        this.toastr.error('El nombre de usuario ya existe.');
-        this.userService.user_logged = resp.data_user;
-      } else {
-        // Si el usuario no existe, registrar al usuario
-        this.userService.postRegister(user).subscribe(
-          (resp: string) => {
-            console.log(resp);
-            this.userService.user_noLoged = user;
-            this.userService.user_noLoged.user_id = Number(resp);
-            this.toastr.success('Registro exitoso. Por favor inicie sesión.');
-            this.router.navigate(['/login']);
-          }
-        );
+    // Verificar si el nombre de usuario ya existe
+    this.userService.usuarioEncontrado(user.username).subscribe(
+      (resp: any) => {
+        if (resp && resp.data_user && resp.data_user.length > 0) {
+          console.log(resp);
+          this.toastr.error('El nombre de usuario ya existe.');
+          this.userService.user_logged = resp[0];
+        } else {
+          // Si el usuario no existe, registrar al usuario
+          this.userService.postRegister(user).subscribe(
+            (resp: string) => {
+              console.log(resp);
+              this.userService.user_noLoged = user;
+              this.userService.user_noLoged.user_id = Number(resp);
+              this.toastr.success('Registro exitoso. Por favor inicie sesión.');
+              this.router.navigate(['/login']);
+            }
+          );
+        }
       }
-    }
-  );
-}
+    );
+  }
 
-  
-    private buildForm(){
+
+  private buildForm() {
     const minPassLength = 8;
-  
+
     this.myForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.pattern(/^@/)]],
       email: ['', [Validators.required, Validators.email]],
@@ -71,11 +71,10 @@ public register() {
       this.isFormValid = this.myForm.valid;
     });
   }
-  
 
-  private checkPasswords(control: AbstractControl)
-  {
-    let resultado = {matchPassword: true};
+
+  private checkPasswords(control: AbstractControl) {
+    let resultado = { matchPassword: true };
 
     // console.log(control.parent);
     // console.log(control.parent?.value);
